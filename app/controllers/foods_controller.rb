da6fd9cfedec75f0e058a61d65f8b84c92ccf595
arnaud rigourd <arnaud.rigourd@hotmail.fr>
2023-01-04 18:57:06 +0100

    Add research by name instead of barcode

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -21,12 +21,13 @@ class FoodsController < ApplicationController
 
   def create
     @food = Food.new(food_params)
-    # set_api
-    # @food.name = @food_h['product']['generic_name']
-    # @food_calories = @food_h['product']['nutriments']['energy-kcal_100g']
-    # @food_calories_unit = @food_h['product']['nutriments']['energy-kcal_unit']
 
-    code = @food.code
+    @products = Openfoodfacts::Product.search(@food.name)
+
+    @product = @products.first
+
+    @food.code = @product.code
+    code = @product.code
     @product = Openfoodfacts::Product.get(code, locale: 'fr')
     @food.name = @product.product_name
     @food.calories = @product.nutriments.to_hash['energy-kcal_100g']
@@ -39,12 +40,6 @@ class FoodsController < ApplicationController
   end
 
   private
-
-  # def set_api
-  #   url = "https://world.openfoodfacts.org/api/v0/product/#{@food.code}.json"
-  #   food_serialized = URI.open(url).read
-  #   @food_h = JSON.parse(food_serialized)
-  # end
 
   def food_params
     params.require(:food).permit(:name, :calories, :code, :quantity)
